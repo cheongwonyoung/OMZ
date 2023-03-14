0fa135c98967b75d42801fd312dfc566138a02ed
chaelin <chaedev3@gmail.com>
2023-03-14 17:37:58 +0900

    [FE][Feat]: community 수정

--- Development/FE/omz/src/components/communityPage/CommunityArticleItem.tsx
+++ Development/FE/omz/src/components/communityPage/CommunityArticleItem.tsx
@@ -1,5 +1,66 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEllipsis,
+  faComment,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import { images } from "../../assets/images";
+
 const CommunityArticleItem: React.FC<{ content: string }> = (props) => {
-  return <li>{props.content}</li>;
+  return (
+    <div className="w-full flex justify-center">
+      <div className="flex justify-center items-start w-10/12 relative py-2 my-1 bg-white/70 rounded-xl">
+        <img
+          className="flex-grow-0 flex-shrink-0 w-1/12"
+          src={images.mini_room_img}
+        />
+        <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0">
+          <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative pl-2.5">
+            <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0">
+              <div className="flex justify-between items-start self-stretch flex-grow-0 flex-shrink-0 relative">
+                <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 relative gap-2.5 py-1">
+                  <p className="flex-grow-0 flex-shrink-0 text-sm font-medium text-left">
+                    닉네임
+                  </p>
+                </div>
+                {/* TODO: 이거는 내가 썼을 때만 나타나게 하기  */}
+                <FontAwesomeIcon icon={faEllipsis} />
+              </div>
+
+              <div className="flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 py-2.5">
+                <p className="flex-grow-0 flex-shrink-0 text-sm text-left text-black">
+                  내용내용내용
+                </p>
+              </div>
+            </div>
+            <img
+              className="flex-grow-0 flex-shrink-0 w-[281px] h-[138px] rounded-[10px]"
+              src={images.main_logo}
+            />
+          </div>
+          <div className="flex justify-start items-end self-stretch flex-grow-0 flex-shrink-0 gap-[90px] pl-2.5 pt-2.5">
+            <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 relative">
+              <FontAwesomeIcon icon={faComment} />
+              <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 relative gap-2.5 pl-[5px] pr-[15px]">
+                <p className="flex-grow-0 flex-shrink-0 text-sm text-left">
+                  123
+                </p>
+              </div>
+              <FontAwesomeIcon icon={faHeart} />
+              <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-2.5 pl-[5px] pr-[15px]">
+                <p className="flex-grow-0 flex-shrink-0 text-sm text-lef">12</p>
+              </div>
+            </div>
+            <div className="flex justify-end items-center flex-grow-0 flex-shrink-0 h-6 relative gap-2.5">
+              <p className="flex-grow-0 flex-shrink-0 text-sm text-lef">
+                Timestamp
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommunityArticleItem;
--- Development/FE/omz/src/pages/CommunityPage.tsx
+++ Development/FE/omz/src/pages/CommunityPage.tsx
@@ -1,6 +1,7 @@
 import CommunityBanner from "../components/communityPage/CommunityBanner";
 import CommunityNavbar from "../components/communityPage/CommunityNavbar";
-import CommunityArticles from "../components/communityPage/communityArticles";
+import CommunityArticles from "../components/communityPage/CommunityArticles";
+import CommunityPopularArticle from "../components/communityPage/CommunityPopularArticle";
 import Article from "../api/types";
 
 export default function CommunityPage() {
@@ -8,8 +9,13 @@ export default function CommunityPage() {
   return (
     <div>
       <CommunityBanner />
-      {/* TODO: 나중에 api에서 가져온 데이터들 삭 가져오기  */}
-      <CommunityArticles items={articles} />
+      <div>
+        <p className="p-2">실시간 인기글</p>
+        <CommunityPopularArticle />
+        <p className="p-2">최신글</p>
+        {/* TODO: 나중에 api에서 가져온 데이터들 삭 가져오기  */}
+        <CommunityArticles items={articles} />
+      </div>
       <CommunityNavbar />
     </div>
   );
