ffb2be599b7efe0fe9c909b6ef6f7655c8e6fd4e
10yutae29 <10yutae29@gmail.com>
2023-03-14 22:27:56 +0900

    [FE][Feat]:선호하는 동물상 인풋 받기

--- Development/FE/omz/src/components/signUp/NextBtn.tsx
+++ Development/FE/omz/src/components/signUp/NextBtn.tsx
@@ -1,11 +1,15 @@
 type Props = {
   comment: string;
   icon?: any;
+  logic(): void;
 };
 
-export default function NextBtn({ comment, icon }: Props) {
+export default function NextBtn({ comment, icon, logic }: Props) {
   return (
-    <div className="flex justify-center items-center px-6 py-2 border-solid border border-black rounded-md bg-orange-200 opacity-90">
+    <div
+      onClick={logic}
+      className="flex justify-center items-center px-6 py-2 border-solid border border-black rounded-md bg-orange-200 opacity-90 cursor-pointer"
+    >
       <p className="text-black">
         {comment}
         {icon}
--- Development/FE/omz/src/components/signUp/StepFaceResult.tsx
+++ Development/FE/omz/src/components/signUp/StepFaceResult.tsx
@@ -1,16 +1,58 @@
 import { Model } from "../../assets/3DAvatar/Rabbit";
 import Camera3D from "../common/Camera3D";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NextBtn from "./NextBtn";
 
 type Props = {
   animal: string;
+  getNickname(e: any): void;
+  nickname: string;
+  plusPage(): void;
 };
 
-export default function StepFaceResult({ animal }: Props) {
+export default function StepFaceResult({
+  animal,
+  getNickname,
+  nickname,
+  plusPage,
+}: Props) {
   const showAvatar = () => {
     switch (animal) {
       case "rabbit":
         return <Camera3D Avatar={<Model position={[0, -2, 0]} />} />;
     }
   };
-  return <div className="w-full h-screen">{showAvatar()}</div>;
+
+  const content = () => {
+    switch (animal) {
+      case "rabbit":
+        return "상큼발랄한 매력의 토끼상";
+    }
+  };
+
+  return (
+    <div className="w-full h-screen flex flex-col items-center">
+      <div>
+        <p className="text-lg font-bold">나는....</p>
+        <p className="text-lg text-violet-500 font-bold">{content()}</p>
+      </div>
+      <div className="h-2/4 w-2/4 ">{showAvatar()}</div>
+      <p className="font-bold mb-8">나만의 닉네임을 입력하세요(최대10자)</p>
+      <input
+        type="text"
+        value={nickname}
+        onChange={(e) => getNickname(e)}
+        maxLength={10}
+        className="bg-transparent border-b border-solid border-black outline-none mb-8"
+      />
+      {nickname.length > 0 && (
+        <NextBtn
+          comment={"다음 스텝으로"}
+          icon={<FontAwesomeIcon icon={faArrowRight} />}
+          logic={plusPage}
+        />
+      )}
+    </div>
+  );
 }
