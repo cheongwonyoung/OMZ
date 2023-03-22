14501908d155e0fd5dc5bdc9afeec78f804da259
chaelin <chaedev3@gmail.com>
2023-03-22 17:28:45 +0900

    [FE][Feat]: Community API 연결중
    Former-commit-id: af5be69c064a53b01421c817ebbad1916ec9e06a

--- Development/FE/omz/src/pages/CommunityDetailPage.tsx
+++ Development/FE/omz/src/pages/CommunityDetailPage.tsx
@@ -3,34 +3,50 @@ import CommunityBanner from "../components/communityPage/CommunityBanner";
 import CommunityCommentInput from "../components/communityPage/CommunityCommentInput";
 import CommunityComment from "../components/communityPage/CommunityComment";
 import { getArticle } from "../api/community";
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 // import CommunityNavbar from "../components/communityPage/CommunityNavbar";
+import { useLocation } from "react-router-dom";
+import { useQuery, useMutation } from "react-query";
+import { createReply } from "../api/community";
 
-type Board = {
+type Comment = {
   [key: string]: any;
 };
 
 export default function CommunityDetailPage() {
-  const param = Number(useParams().board_id);
-  const memberId = 1;
-  const [data, setData] = useState(null);
+  const location = useLocation();
+  const boardId = location.state.boardId;
+  const memberId = location.state.memberId;
+  const { data, isLoading, isError, error, refetch } = useQuery("article", () =>
+    getArticle(boardId, memberId)
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getArticle(param, memberId);
-      setData(result.data);
-    };
-    fetchData();
-  }, [param, memberId]);
+  const addComment = useMutation(
+    (comment: { boardId: number; content: string; memberId: number }) =>
+      createReply(comment),
+    {
+      onSuccess: () => {
+        refetch();
+      },
+    }
+  );
+
+  const handleCommentSubmit = (comment: string) => {
+    addComment.mutate({ boardId, content: comment, memberId });
+  };
+
+  if (isLoading) return <h3>Loading..</h3>;
+  if (isError) return <h3>Error...</h3>;
 
   return (
     <div>
       <CommunityBanner />
-      <CommunityArticleItem key={uuidv4()} item={data} />
-      <CommunityCommentInput />
-      <CommunityComment />
+      <CommunityArticleItem key={uuidv4()} item={data?.data} />
+      <CommunityCommentInput onCommentSubmit={handleCommentSubmit} />
+
+      {data?.data.replies.map((comment: Comment) => (
+        <CommunityComment key={uuidv4()} item={comment} refetch={refetch} />
+      ))}
       {/* <CommunityNavbar /> */}
     </div>
   );
