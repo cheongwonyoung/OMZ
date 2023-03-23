f8e5b2a3d092c5e78886bd1ab60a246d62cdee6e
10yutae29 <10yutae29@gmail.com>
2023-03-23 10:26:03 +0900

    [FE][Feat]: 마이페이지 수정
    Former-commit-id: 09bc87945922cfbad12180a9b304ae3029bb5d6d

--- Development/FE/omz/src/components/mypage/UpdateAnimal.tsx
+++ Development/FE/omz/src/components/mypage/UpdateAnimal.tsx
@@ -1,10 +1,26 @@
-import { images } from "../../assets/images";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { images } from "../../assets/images";
 
 export default function UpdateAnimal() {
   const animalList = ["강아지", "고양이", "곰", "여우", "토끼", "공룡"];
+  interface AnimalPrefer {
+    [key: string]: number;
+  }
+  const [animalPrefer, setAnimalPrefer] = useState<AnimalPrefer>({
+    강아지: 0,
+    고양이: 0,
+    곰: 0,
+    여우: 0,
+    토끼: 0,
+    공룡: 0,
+  });
 
+  const changePrefer = (e: any) => {
+    const name = e.target.id;
+    const value = e.target.value;
+    setAnimalPrefer({ ...animalPrefer, [name]: value });
+    console.log(animalPrefer);
+  };
   const imgsrc = (i: string): string => {
     switch (i) {
       case "강아지":
@@ -23,31 +39,10 @@ export default function UpdateAnimal() {
         return "";
     }
   };
-
-  interface AnimalPrefer {
-    [key: string]: number;
-  }
-
-  const [animalPrefer, setAnimalPrefer] = useState<AnimalPrefer>({
-    강아지: 0,
-    고양이: 0,
-    곰: 0,
-    여우: 0,
-    토끼: 0,
-    공룡: 0,
-  });
-
-  const changePrefer = (e: any) => {
-    const name = e.target.id;
-    const value = e.target.value;
-    setAnimalPrefer({ ...animalPrefer, [name]: value });
-    console.log(animalPrefer);
-  };
-
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       {animalList.map((item) => (
-        <div key={uuidv4()} className="grid grid-cols-4 gap-4 items-center">
+        <div className="grid grid-cols-4 gap-4 items-center">
           <div className="flex flex-col items-center">
             <img
               src={images[imgsrc(item)]}
