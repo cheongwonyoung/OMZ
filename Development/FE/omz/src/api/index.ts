49be562f97b0741ec1510b06b4221b33da5f9ecb
강윤주 <jjjoody799@gmail.com>
2023-03-27 16:32:18 +0900

    [BE][Feat]: 배포
    Former-commit-id: f100e24e4d6634d2a80faba58cba90a47673b492

--- Development/FE/omz/src/api/index.ts
+++ Development/FE/omz/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
   // baseURL: 'http://127.0.0.1:8080/api', 
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://j8a705.p.ssafy.io:8080/api',
   headers: { 
     "Content-Type": "application/json", 
 },
