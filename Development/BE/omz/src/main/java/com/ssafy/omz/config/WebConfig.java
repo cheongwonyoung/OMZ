16ba03b1cd7d35b1170ea66ab41250b361eddbbb
강윤주 <jjjoody799@gmail.com>
2023-03-28 17:54:03 +0900

    [BE][Feat]: 배포
    Former-commit-id: a174fe0b64586f59a7f47931551e4c2857a420a1

--- Development/BE/omz/src/main/java/com/ssafy/omz/config/WebConfig.java
+++ Development/BE/omz/src/main/java/com/ssafy/omz/config/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://j8a705.p.ssafy.io:5173")
+                .allowedOrigins("http://j8a705.p.ssafy.io:80")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 
