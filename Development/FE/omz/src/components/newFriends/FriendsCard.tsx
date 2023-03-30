10ea8d9cef8d833ae743b289cded47c957a63570
10yutae29 <10yutae29@gmail.com>
2023-03-30 20:19:29 +0900

    [FE][Feat]:친구 추천 로직 완료
    Former-commit-id: ffc6bd436b2eaaa8384db760547adb5d9dbfe57a

--- Development/FE/omz/src/components/newFriends/FriendsCard.tsx
+++ Development/FE/omz/src/components/newFriends/FriendsCard.tsx
@@ -8,19 +8,39 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import TagList from "./TagList";
 import FriendBtn from "./FriendBtn";
+import { useNavigate } from "react-router-dom";
 type Props = {
   handleRefuseModal(): void;
   handleProposalModal(): void;
   handleModalFor(memberId: number, nickname: string): void;
+  info: any;
 };
 export default function FriendsCard({
   handleRefuseModal,
   handleProposalModal,
   handleModalFor,
+  info,
 }: Props) {
   // const tags = ["곰상", "ISFP", "짝꿍", "가능성", "95%"];
-  const info_tags = ["곰상", "ISFP"];
-  const possibility_tags = ["100%", "가능성", "짝꿍"];
+  const info_tags = [`${info.animal}상`, `${info.mbti}`];
+  const possibility_tags = [
+    `${Math.ceil(info.result * 100)}%`,
+    "가능성",
+    "짝꿍",
+  ];
+
+  const goProposal = () => {
+    handleModalFor(info.memberId, info.nickname);
+    handleProposalModal();
+  };
+
+  const goReject = () => {
+    handleModalFor(info.memberId, info.nickname);
+    handleRefuseModal();
+  };
+
+  const navigate = useNavigate();
+
   return (
     <div className="w-full bg-slate-500/50 rounded-2xl flex flex-col items-center gap-5 pt-10 pb-20">
       {/* <CameraAvatar Avatar={<Bear />} /> */}
@@ -28,12 +48,15 @@ export default function FriendsCard({
       <div className="h-60">
         <CameraAvatar Avatar={<Model />} />
       </div>
-      <div className="text-2xl text-white">윾태초이</div>
+      <div className="text-2xl text-white">{info.nickname}</div>
       <div className="flex flex-col gap-2">
         <TagList tags={info_tags} />
         <TagList tags={possibility_tags} />
       </div>
-      <button className="text-white text-base opacity-95 hover:text-black">
+      <button
+        className="text-white text-base opacity-95 hover:text-black"
+        onClick={() => navigate(`/miniroom/${info.memberId}`)}
+      >
         <FontAwesomeIcon icon={faHome} className="mr-2" />
         놀러가기
       </button>
@@ -41,12 +64,12 @@ export default function FriendsCard({
         <FriendBtn
           icon={<FontAwesomeIcon icon={faHeart} />}
           text={"친구 신청"}
-          logic={handleProposalModal}
+          logic={goProposal}
         />
         <FriendBtn
           icon={<FontAwesomeIcon icon={faHeartCrack} />}
           text={"친구 거절"}
-          logic={handleRefuseModal}
+          logic={goReject}
         />
       </div>
     </div>
