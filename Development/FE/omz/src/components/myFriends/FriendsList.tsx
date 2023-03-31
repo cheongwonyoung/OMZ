37c161a982115538cd708b7b6c08f633e7788118
10yutae29 <10yutae29@gmail.com>
2023-03-31 10:07:22 +0900

    [FE][Feat]:친구목록 에러 해결
    Former-commit-id: fae34a491b8e7dcabf71b29fcfc4f1bfd6cfb644

--- Development/FE/omz/src/components/myFriends/FriendsList.tsx
+++ Development/FE/omz/src/components/myFriends/FriendsList.tsx
@@ -1,10 +1,6 @@
-import { useQuery } from "react-query";
-import { getMyFriendsList } from "../../api/myFriends";
 import FriendsItem from "./FriendsItem";
 import ListBottomBar from "./ListBottomBar";
 import { v4 as uuidv4 } from "uuid";
-import { useRecoilValue } from "recoil";
-import { userStatus } from "../../recoil/userAtom";
 
 type Props = {
   handleDeleteMember(name: string, id: number): void;
@@ -12,17 +8,6 @@ type Props = {
 };
 
 export default function FriendsList({ handleDeleteMember, friends }: Props) {
-  // TODO 여기 멤버아이디 로그인한 유저로 바꾸고 나중에 DB 생기면 그때 변경
-  const memberId = useRecoilValue(userStatus).id;
-  // const { data: friends, refetch } = useQuery(
-  //   "friendlist",
-  //   () => getMyFriendsList(memberId),
-  //   {
-  //     onSuccess(data) {
-  //       console.log(data);
-  //     },
-  //   }
-  // );
   return (
     <div className="w-full flex flex-col items-center">
       {friends !== undefined &&
