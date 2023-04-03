345712bc9e36bf70274e8feb914c8cdfbf4f1f86
chaelin <chaedev3@gmail.com>
2023-04-03 16:58:01 +0900

    [FE][Feat]: 채팅 말걸기 수정
    Former-commit-id: c7c6792df41f44104859f4fc2588bccc10e87dea

--- Development/FE/omz/src/components/newFriends/FriendsCard.tsx
+++ Development/FE/omz/src/components/newFriends/FriendsCard.tsx
@@ -4,6 +4,7 @@ import {
   faHome,
   faHeart,
   faHeartCrack,
+  faMessage,
 } from "@fortawesome/free-solid-svg-icons";
 import TagList from "./TagList";
 import FriendBtn from "./FriendBtn";
@@ -14,18 +15,21 @@ import { Custom_fox } from "../../assets/3DAvatar/Custom_fox";
 import { Custom_dino } from "../../assets/3DAvatar/Custom_dino";
 import { Custom_cat } from "../../assets/3DAvatar/Custom_cat";
 import { Custom_dog } from "../../assets/3DAvatar/Custom_dog";
+import { talkToFriends } from "../../api/chatting";
 
 type Props = {
   handleRefuseModal(): void;
   handleProposalModal(): void;
   handleModalFor(memberId: number, nickname: string): void;
   info: any;
+  handletalkFriends(id: number): void;
 };
 export default function FriendsCard({
   handleRefuseModal,
   handleProposalModal,
   handleModalFor,
   info,
+  handletalkFriends,
 }: Props) {
   // const tags = ["곰상", "ISFP", "짝꿍", "가능성", "95%"];
   const info_tags = [`${info.animal}상`, `${info.mbti}`];
@@ -126,6 +130,13 @@ export default function FriendsCard({
         <TagList tags={info_tags} />
         <TagList tags={possibility_tags} />
       </div>
+      <button
+        className="text-white text-base opacity-95 hover:text-black"
+        onClick={() => handletalkFriends(info.memberId)}
+      >
+        <FontAwesomeIcon className="text-pink-400" icon={faMessage} />
+        말걸기
+      </button>
       <button
         className="text-white text-base opacity-95 hover:text-black"
         onClick={() => navigate(`/miniroom/${info.memberId}`)}
